e8f09800352570e1bb76ef94085b7232c67cb85b
Isomukhammad <s.isomuxammad@gmail.com>
2022-01-17 21:12:33 +0500

    first commit

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ const $$ = document.querySelectorAll.bind(document);
 
 $('#inputs').addEventListener('submit', function (e) {
     e.preventDefault();
-    $$('input').forEach((x) => {
-        console.log(x.nextElementSibling.innerHTML);
-    })
-
     isEveryInput();
 })
 
 function isEveryInput() {
     $$('.input').forEach((e) => {
         if (e.value !== '') {
+            e.style.borderColor = "hsl(246, 25%, 77%)";
+            e.style.backgroundImage = "none";
             e.nextElementSibling.style.display = 'none';
+
+            if (e.type == 'email') {
+                e.placeholder = "email@example/com";
+                e.classList.remove('red');
+            }
         } else {
+            e.style.borderColor = "hsl(0, 100%, 74%)";
+            e.style.backgroundImage = "url('./images/icon-error.svg')";
             e.nextElementSibling.style.display = 'flex';
+
+            if (e.type == 'email') {
+                e.placeholder = "email@example/com";
+                e.classList.add('red');
+            }
         }
     })
 }
